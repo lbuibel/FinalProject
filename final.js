87c08af5fa610365afcc2986a4a90e0d43d54e70
Logan Uibel <lbuibel@gmail.com>
2018-11-19 17:41:51 -0700

    added senators/pokemon/starwars api info -- set up main js page

---
+++ final.js
@@ -0,0 +1,8 @@
+import { people } from './assets/starwarsPeople.js'
+import { senators } from './assets/senator.js'
+import { pokemon } from './assets/pokemon.js'
+
+console.log(`Star wars people import: ${people}`)
+console.log(`Senators import: ${senators}`)
+console.log(`Pokemon import: ${pokemon}`)
+
