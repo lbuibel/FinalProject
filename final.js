b95196425cd6264815a6010f7da11f3c935cdb86
Logan Uibel <lbuibel@gmail.com>
2018-11-21 19:54:55 -0700

    filter function for pokemon types

--- final.js
+++ final.js
@@ -6,3 +6,22 @@ console.log(`Star wars people import: ${people}`)
 console.log(`Senators import: ${senators}`)
 console.log(`Pokemon import: ${pokemon}`)
 
+const electricType = pokemon.filter(pokemon => pokemon.type[0] === "electric");
+const fireType = pokemon.filter(pokemon => pokemon.type[0] === "fire");
+const waterType = pokemon.filter(pokemon => pokemon.type[0] === "water")
+const grassType = pokemon.filter(pokemon => pokemon.type[0] === "grass")
+
+/*
+const fireAttack = pokemon.reduce((strongest, pokemon) => {
+    return (strongest[0].base.Attack || 0) > fireType[0].base.Attack ? strongest : pokemon
+}, {})
+*/
+
+
+
+console.log(electricType[0].base.Attack)
+console.log(fireType)
+console.log(waterType)
+console.log(grassType)
+
+console.log(fireAttack)
