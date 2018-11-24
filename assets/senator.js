87df7ed9367c31a1b5553d3ce415f5a3ffd9713a
Logan Uibel <lbuibel@gmail.com>
2018-11-24 16:01:04 -0700

    fixed senators import, added highest poke attack outputs

--- assets/senator.js
+++ assets/senator.js
@@ -1,4 +1,4 @@
-export const senators = [
+export const senatorData = [
 
 {
     "status":"OK",
--- final.js
+++ final.js
@@ -1,27 +1,25 @@
 import { people } from './assets/starwarsPeople.js'
-import { senators } from './assets/senator.js'
+import { senatorData } from './assets/senator.js'
 import { pokemon } from './assets/pokemon.js'
 
-console.log(`Star wars people import: ${people}`)
-console.log(`Senators import: ${senators}`)
-console.log(`Pokemon import: ${pokemon}`)
+
+const senators = senatorData[0].results[0].members
+
 
 const electricType = pokemon.filter(pokemon => pokemon.type[0] === "electric");
 const fireType = pokemon.filter(pokemon => pokemon.type[0] === "fire");
 const waterType = pokemon.filter(pokemon => pokemon.type[0] === "water")
 const grassType = pokemon.filter(pokemon => pokemon.type[0] === "grass")
 
-/*
-const fireAttack = pokemon.reduce((strongest, pokemon) => {
-    return (strongest[0].base.Attack || 0) > fireType[0].base.Attack ? strongest : pokemon
-}, {})
-*/
+
+const fireAttack = fireType.reduce((acc, pokemon) => fireType[0].base.Attack > 0 ? pokemon : acc, 0)
+console.log(`highest fire attack: ${fireAttack.ename}`)
+
+const waterAttack = waterType.reduce((acc, pokemon) => waterType[0].base.Attack > 0 ? pokemon : acc, 0)
+console.log(`highest water attack: ${waterAttack.ename}`)
 
 
-
-console.log(electricType[0].base.Attack)
 console.log(fireType)
 console.log(waterType)
 console.log(grassType)
 
-console.log(fireAttack)
